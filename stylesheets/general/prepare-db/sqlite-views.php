c06f499fda834b762b56a939f4c5bcf5cdce9e73
Pavel Klinger <klinger@rhea.bigr.cz>
2013-04-17 23:47:24 +0200

    .

--- stylesheets/general/prepare-db/sqlite-views.php
+++ stylesheets/general/prepare-db/sqlite-views.php
@@ -356,6 +356,8 @@ $definitions = array (
 
 ?>
 
+SELECT load_extension('libspatialite.so');
+
 REPLACE INTO spatial_ref_sys (srid, auth_name, auth_srid, ref_sys_name, proj4text) values (900913,'EPSG',900913,'Google Maps Global Mercator','+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs');
 
 <?php foreach ( $definitions as $name => $def  ): ?>
