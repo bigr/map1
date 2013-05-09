594455f64c6ba0d4f1b0e08ff4922edd2bb3d5ba
Pavel Klinger <klinger@rhea.bigr.cz>
2013-05-09 09:59:00 +0200

    Version 0.4

--- stylesheets/general/prepare-db/get-sql-rect.php
+++ stylesheets/general/prepare-db/get-sql-rect.php
@@ -5,7 +5,7 @@
     $LON_END = $argv[3] + ($argv[3] - $argv[1]) * 0.15;
     $LAT_END = $argv[4] + ($argv[4] - $argv[2]) * 0.05;
 
-    $TILE = $argv[5] . '_' . $argv[6];
+    $POSTFIX = $argv[5] . '_' . $argv[6];
 
     if ( !defined('ROOT') ) {
         define('ROOT',dirname(dirname(dirname(__FILE__))));
@@ -24,6 +24,6 @@
     
     foreach ( $sqls as $sql ) {
         require_once "sql/{$sql[0]}.sql.php";
-        echo "DROP TABLE IF EXISTS {$sql[1]}_$TILE;\n";    
-        echo "CREATE TABLE {$sql[1]}_$TILE AS (SELECT * FROM (" . $sql[2]() . ") T WHERE ST_Intersects(way,ST_Transform(ST_SetSRID(ST_MakeBox2D(ST_GeomFromEWKT('SRID=4326;POINT($LON_START $LAT_START)'), ST_GeomFromEWKT('SRID=4326;POINT($LON_END $LAT_END)')), 4326),900913)));\n";
+        echo "DROP TABLE IF EXISTS {$sql[1]}_$POSTFIX;\n";    
+        echo "CREATE TABLE {$sql[1]}_$POSTFIX AS (SELECT * FROM (" . $sql[2]() . ") T WHERE ST_Intersects(way,ST_Transform(ST_SetSRID(ST_MakeBox2D(ST_GeomFromEWKT('SRID=4326;POINT($LON_START $LAT_START)'), ST_GeomFromEWKT('SRID=4326;POINT($LON_END $LAT_END)')), 4326),900913)));\n";
     }
